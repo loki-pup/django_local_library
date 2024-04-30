60c898d3aadcb1d0e315b4f8f859df275a9f8f72
lokifc <132177231+lokifc@users.noreply.github.com>
2024-04-30 17:57:20 +0800

    deploy

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -42,6 +42,14 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['lokiii.pythonanywhere.com', '127.0.0.1']
 
+## For example, for a site URL is at 'web-production-3640.up.railway.app'
+## (replace the string below with your own site URL):
+CSRF_TRUSTED_ORIGINS = ['https://lokiii.pythonanywhere.com']
+
+# During development/for this tutorial you can instead set just the base URL
+# CSRF_TRUSTED_ORIGINS = ['https://*.pythonanywhere.com']
+
+
 # Application definition
 
 INSTALLED_APPS = [
